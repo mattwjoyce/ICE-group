eb65659220b50b1079add27864d2b796d7af96ba
daisyismydog <46864272+daisyismydog@users.noreply.github.com>
2020-05-23 00:49:38 +1000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from './data-service.service';
 import { Team } from './team';
-
-
+import { FavouriteTeamService } from './favourite-team.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Team Explorer Pro';
   
   favouriteTeam:Team; 
   teams:Team[];
 
-  constructor(private dataService: DataServiceService) { }
+  constructor(private dataService: DataServiceService, private FavouriteTeamService:FavouriteTeamService) { }
+
 
   ngOnInit() {
     this.getAFLTeams();
+  // data for other components 
+    this.FavouriteTeamService.getFavouriteTeam().subscribe(team => this.favouriteTeam = team);
   }
 
   onSelect(team: Team): void{
     this.favouriteTeam = team;
-
+    this.FavouriteTeamService.setFavouriteTeam(this.favouriteTeam); 
   }
+
+
 
   getAFLTeams(): void {
     this.dataService.getTeams().subscribe(temp => { this.teams = temp;});
